cfedeeb69ddecf054bd5b45ac11573b205492fc9
Rafael Teixeira <rasfaeleduardoteixeira@gmail.com>
2022-02-02 08:31:04 -0300

    Initial state login

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Footer,
   LoginHeader,
@@ -6,21 +6,43 @@ import {
   FormStatus,
 } from '@/presentation/components'
 import Styles from './login-styles.scss'
+import formContext from '@/presentation/components/context/form/form-context'
 
 const Login: React.FC = () => {
+  const [stateLogin, setStateLogin] = useState({
+    isLoading: false,
+  })
+
+  const [stateErrorLogin, setStateErrorLogin] = useState({
+    errorMessage: '',
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+  })
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <form className={Styles.form}>
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
-        <span className={Styles.link}>Criar conta</span>
-        <FormStatus />
-      </form>
+      <formContext.Provider value={{ stateErrorLogin, stateLogin }}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button
+            className={Styles.submit}
+            type="submit"
+            data-testid="submitButton"
+            disabled
+          >
+            Entrar
+          </button>
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </formContext.Provider>
       <Footer />
     </div>
   )
